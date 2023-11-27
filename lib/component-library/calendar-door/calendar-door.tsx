c684d4c219ee73f8854f4be2bd8b06c593b50dd6
Reece <reecemorgandev@gmail.com>
2023-11-27 22:47:59 +0000

    Create calendar-door.tsx

---
+++ lib/component-library/calendar-door/calendar-door.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { ReactNode } from 'react';
+import Link from "next/link";
+
+interface Props {
+    children: ReactNode;
+    link: string;
+    isComplete: boolean;
+}
+
+export const CalendarDoor = ({ children, link, isComplete }: Props) => {
+    return (
+        <Wrapper>
+            {isComplete ? (
+            <StyledLink href={link}>
+                {children}
+            </StyledLink>
+            ) : (
+            <DisabledLink>
+                {children}
+            </DisabledLink>
+            )}
+            
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    height: 100px;
+    width: 75px;
+    font-size: 3em;
+    font-weight: 800;
+    margin: 15px;
+`;
+
+const StyledLink = styled(Link)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #1f9c0e;
+    background-color: #6b0c0c;
+    border-radius: 50px 50px 0 0;
+    height: 100%;
+    width: 100%;
+    cursor: pointer;
+`;
+
+const DisabledLink = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #6a6e69;
+    background-color: #414540;
+    border-radius: 50px 50px 0 0;
+    height: 100%;
+    width: 100%;
+    cursor: not-allowed;
+`;
