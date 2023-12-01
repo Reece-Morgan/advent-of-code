940cc5f34d27087a064e51bbb352dac02cf44735
Reece <reecemorgandev@gmail.com>
2023-12-01 20:15:32 +0000

    Create day-2.tsx

---
+++ pages/2023/day-2.tsx
@@ -0,0 +1,32 @@
+import { Title } from "@aoc/components"
+import styled from "styled-components";
+
+const TwentyTwentyThree_DayTwo = () => {
+    const partOne = () => {
+        // Function for Part One
+    }
+
+    const partTwo = () => {
+        // Function for Part Two
+    }
+   
+    return (
+        <>
+            <Title>2023 - Day 2</Title>
+            <Subtitle>Part 1</Subtitle>
+            {/* <Answer>{partOne()}</Answer>
+            <Subtitle>Part 2</Subtitle>
+            <Answer>{partTwo()}</Answer> */}
+        </>
+    )
+}
+
+export default TwentyTwentyThree_DayTwo
+
+const Subtitle = styled.h2`
+    text-align: center;
+`;
+
+const Answer = styled.div`
+    text-align: center;
+`;
