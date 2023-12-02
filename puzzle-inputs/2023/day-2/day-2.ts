c8e20d981ba26acf44bea2155a8155e63c579aa5
Reece <reecemorgandev@gmail.com>
2023-12-02 11:15:38 +0000

    day 2 complete

--- puzzle-inputs/2023/day-2/day-2.ts
+++ puzzle-inputs/2023/day-2/day-2.ts
@@ -1,4 +1,4 @@
-export const fakeData1 = `
+export const testData = `
     Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
     Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
