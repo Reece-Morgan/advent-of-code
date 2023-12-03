2a53eb267da81596cb028565d97c0633af7248ec
Reece <reecemorgandev@gmail.com>
2023-12-03 21:13:12 +0000

    updated day template

--- templates/day-x.tsx
+++ templates/day-x.tsx
@@ -1,27 +1,29 @@
 import { Title } from "@aoc/components"
 import styled from "styled-components";
 
-const TwentyTwentyThree_DayXxx = () => {
+const TwentyTwentyThree_DayX = () => {
     const partOne = () => {
         // Function for Part One
+        return 'imcomplete'
     }
 
     const partTwo = () => {
         // Function for Part Two
+        return 'imcomplete'
     }
    
     return (
         <>
             <Title>2023 - Day X</Title>
             <Subtitle>Part 1</Subtitle>
-            {/* <Answer>{partOne()}</Answer>
+            <Answer>{partOne()}</Answer>
             <Subtitle>Part 2</Subtitle>
-            <Answer>{partTwo()}</Answer> */}
+            <Answer>{partTwo()}</Answer>
         </>
     )
 }
 
-export default TwentyTwentyThree_DayXxx
+export default TwentyTwentyThree_DayX
 
 const Subtitle = styled.h2`
     text-align: center;
