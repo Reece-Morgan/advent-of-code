1f931baeacea0b7888202eb0cc2ebccc72f32deb
Reece <reecemorgandev@gmail.com>
2023-12-04 19:17:50 +0000

    WiP part 2

--- pages/2023/day-4.tsx
+++ pages/2023/day-4.tsx
@@ -29,8 +29,79 @@ const TwentyTwentyThree_DayFour = () => {
         return totalSum;
     }
 
-    const partTwo = () => {
-        // Function for Part Two
+    type Card = {
+        cardId: number;
+        iterations: number;
+    }
+
+    const initializeCardData = (lines: string[]) => {
+        const cardData: Card[] = []
+        for (let i = 0; i < lines.length; i++) {
+            const lineId = i + 1;
+            const card = {cardId: lineId, iterations: 1}
+            cardData.push(card);
+        }
+        return cardData;
+    }
+
+    const partTwo = (data: string) => {
+        let totalScratchcards = 0;
+        const originalLines = data.trim().split(/\n/g);
+        const newLines = data.trim().split(/\n/g);
+        const cardData = initializeCardData(originalLines);
+        for (let i = 0; i < originalLines.length; i++) {
+            const lineId = i + 1;
+            const line = originalLines[i].substring(originalLines[i].indexOf(':') + 1);
+            const arr = line.split('|');
+            
+            const winningNumbers = arr[0].split(/(\s+)/).filter(function(str) {
+                return /\S/.test(str);
+            });
+            const yourNumbers = arr[1].split(/(\s+)/).filter(function(str) {
+                return /\S/.test(str);
+            });
+
+            const commonNumbers = winningNumbers.filter((number) => yourNumbers.includes(number));
+
+            // console.log('commonNumbers: ', i, ': ', commonNumbers);
+            // console.log('winningNumbers: ', i, ': ', winningNumbers);
+            // console.log('yourNumbers: ', i, ': ', yourNumbers);
+            // Check if there is a next item in the array
+            if (i < originalLines.length - 1) {
+                let dataIndex = 0
+                    for (let d = 0; d < cardData[dataIndex].iterations; d++) {
+                        const id = i + 1;
+                        let dupeLines: string[] = [];
+                        const nextCards = originalLines.slice(id, id + commonNumbers.length);
+                        const lineIds = nextCards.map(str => {
+                            const match = str.match(/\d+/); // Match one or more digits
+                            return match ? parseInt(match[0], 10) : NaN; // Convert the matched string to a number
+                        });
+                        for (let l = 0; l < lineIds.length; l++) {
+                            console.log('surprise muthafucker: ', lineId)
+                        }
+                        dataIndex++;
+                    }
+                // const id = i + 1;
+                // let dupeLines: string[] = [];
+                // const nextCards = originalLines.slice(id, id + commonNumbers.length);
+                // const lineIds = nextCards.map(str => {
+                //     const match = str.match(/\d+/); // Match one or more digits
+                //     return match ? parseInt(match[0], 10) : NaN; // Convert the matched string to a number
+                // });
+                // for (let l = 0; l < lineIds.length; l++) {
+                //     cardData[lineIds[l] - 1].iterations++
+                // }
+                // newLines.splice(i + 1, 0, ...dupeLines);
+            }
+        }
+            
+        console.log('cardData: ', cardData);
+
+        // console.log(originalLines)
+        // console.log(newLines)
+        
+        // return totalScratchcards;
         return 'incomplete'
     }
    
@@ -40,7 +111,7 @@ const TwentyTwentyThree_DayFour = () => {
             <Subtitle>Part 1</Subtitle>
             <Answer>{partOne(dayFourData)}</Answer>
             <Subtitle>Part 2</Subtitle>
-            <Answer>{partTwo()}</Answer>
+            <Answer>{partTwo(testData)}</Answer>
         </>
     )
 }
