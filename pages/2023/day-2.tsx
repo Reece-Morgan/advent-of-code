57b9425a7cabfb2841bb50aab7bbcb270c12d003
Reece <reecemorgandev@gmail.com>
2023-12-04 20:59:40 +0000

    reverted change, still broken build

--- pages/2023/day-2.tsx
+++ pages/2023/day-2.tsx
@@ -1,5 +1,5 @@
 import { Title } from "@aoc/components"
-import { dayTwoData } from "inputs/2023/2";
+import { dayTwoData } from "puzzle-inputs/2023/2";
 import styled from "styled-components";
 
 const TwentyTwentyThree_DayTwo = () => {
