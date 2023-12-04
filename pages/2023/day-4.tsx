ddf00e784f0324de3f35dfce470b9df4777a16de
Reece <reecemorgandev@gmail.com>
2023-12-04 22:50:41 +0000

    WiP

--- pages/2023/day-4.tsx
+++ pages/2023/day-4.tsx
@@ -1,5 +1,6 @@
 import { Title } from "@aoc/components"
 import { dayFourData, testData } from "puzzle-inputs/2023/4";
+import { useState } from "react";
 import styled from "styled-components";
 
 const TwentyTwentyThree_DayFour = () => {
@@ -44,64 +45,110 @@ const TwentyTwentyThree_DayFour = () => {
         return cardData;
     }
 
-    const partTwo = (data: string) => {
-        let totalScratchcards = 0;
-        const originalLines = data.trim().split(/\n/g);
-        const newLines = data.trim().split(/\n/g);
-        const cardData = initializeCardData(originalLines);
-        for (let i = 0; i < originalLines.length; i++) {
-            const lineId = i + 1;
-            const line = originalLines[i].substring(originalLines[i].indexOf(':') + 1);
-            const arr = line.split('|');
+    // const partTwo = (data: string) => {
+    //     let totalScratchcards = 0;
+    //     const originalLines = data.trim().split(/\n/g);
+    //     const newLines = data.trim().split(/\n/g);
+    //     const cardData = initializeCardData(originalLines);
+    //     for (let i = 0; i < originalLines.length; i++) {
+    //         const lineId = i + 1;
+    //         const line = originalLines[i].substring(originalLines[i].indexOf(':') + 1);
+    //         const arr = line.split('|');
             
-            const winningNumbers = arr[0].split(/(\s+)/).filter(function(str) {
-                return /\S/.test(str);
-            });
-            const yourNumbers = arr[1].split(/(\s+)/).filter(function(str) {
-                return /\S/.test(str);
+    //         const winningNumbers = arr[0].split(/(\s+)/).filter(function(str) {
+    //             return /\S/.test(str);
+    //         });
+    //         const yourNumbers = arr[1].split(/(\s+)/).filter(function(str) {
+    //             return /\S/.test(str);
+    //         });
+
+    //         const commonNumbers = winningNumbers.filter((number) => yourNumbers.includes(number));
+
+    //         // console.log('commonNumbers: ', i, ': ', commonNumbers);
+    //         // console.log('winningNumbers: ', i, ': ', winningNumbers);
+    //         // console.log('yourNumbers: ', i, ': ', yourNumbers);
+    //         // Check if there is a next item in the array
+    //         if (i < originalLines.length - 1) {
+    //             let dataIndex = 0
+    //                 for (let d = 0; d < cardData[dataIndex].iterations; d++) {
+    //                     const id = i + 1;
+    //                     let dupeLines: string[] = [];
+    //                     const nextCards = originalLines.slice(id, id + commonNumbers.length);
+    //                     const lineIds = nextCards.map(str => {
+    //                         const match = str.match(/\d+/); // Match one or more digits
+    //                         return match ? parseInt(match[0], 10) : NaN; // Convert the matched string to a number
+    //                     });
+    //                     for (let l = 0; l < lineIds.length; l++) {
+    //                         console.log('surprise muthafucker: ', lineId)
+    //                     }
+    //                     dataIndex++;
+    //                 }
+    //             // const id = i + 1;
+    //             // let dupeLines: string[] = [];
+    //             // const nextCards = originalLines.slice(id, id + commonNumbers.length);
+    //             // const lineIds = nextCards.map(str => {
+    //             //     const match = str.match(/\d+/); // Match one or more digits
+    //             //     return match ? parseInt(match[0], 10) : NaN; // Convert the matched string to a number
+    //             // });
+    //             // for (let l = 0; l < lineIds.length; l++) {
+    //             //     cardData[lineIds[l] - 1].iterations++
+    //             // }
+    //             // newLines.splice(i + 1, 0, ...dupeLines);
+    //         }
+    //     }
+            
+    //     console.log('cardData: ', cardData);
+
+    //     // console.log(originalLines)
+    //     // console.log(newLines)
+        
+    //     // return totalScratchcards;
+    //     return 'incomplete'
+    // }
+
+    const partTwo = (data: string) => {
+        const lines = data.split("\n")
+            .map((line, index) => {
+                const numbersPart = line.split(":")[1];
+                const id = index + 1;
+                const numbersArrays = numbersPart
+                .split("|")
+                .map((numbers) =>
+                    numbers.trim().split(/\s+/).map((numStr) => parseInt(numStr, 10))
+                );
+                
+                return { id, numbersArrays };
             });
 
-            const commonNumbers = winningNumbers.filter((number) => yourNumbers.includes(number));
-
-            // console.log('commonNumbers: ', i, ': ', commonNumbers);
-            // console.log('winningNumbers: ', i, ': ', winningNumbers);
-            // console.log('yourNumbers: ', i, ': ', yourNumbers);
-            // Check if there is a next item in the array
-            if (i < originalLines.length - 1) {
-                let dataIndex = 0
-                    for (let d = 0; d < cardData[dataIndex].iterations; d++) {
-                        const id = i + 1;
-                        let dupeLines: string[] = [];
-                        const nextCards = originalLines.slice(id, id + commonNumbers.length);
-                        const lineIds = nextCards.map(str => {
-                            const match = str.match(/\d+/); // Match one or more digits
-                            return match ? parseInt(match[0], 10) : NaN; // Convert the matched string to a number
-                        });
-                        for (let l = 0; l < lineIds.length; l++) {
-                            console.log('surprise muthafucker: ', lineId)
-                        }
-                        dataIndex++;
-                    }
-                // const id = i + 1;
-                // let dupeLines: string[] = [];
-                // const nextCards = originalLines.slice(id, id + commonNumbers.length);
-                // const lineIds = nextCards.map(str => {
-                //     const match = str.match(/\d+/); // Match one or more digits
-                //     return match ? parseInt(match[0], 10) : NaN; // Convert the matched string to a number
-                // });
-                // for (let l = 0; l < lineIds.length; l++) {
-                //     cardData[lineIds[l] - 1].iterations++
-                // }
-                // newLines.splice(i + 1, 0, ...dupeLines);
+        for (let i = 0; i < lines.length; i++) {
+            const commonNumbers = lines[i].numbersArrays[0].filter((number) => lines[i].numbersArrays[1].includes(number));
+            // console.log(commonNumbers)
+        
+            // Clone the next lines based on the number of common numbers
+            for (let j = 1; j <= commonNumbers.length; j++) {
+                if (i + j < lines.length) {
+                    const lineId = lines[i].id;
+                    console.log(lineId);
+                    const newId = lines[i + j].id
+                    // console.log('id: ', lineId);
+                    // console.log('new id: ', newId);
+                    const newNumbersArrays = [...lines[i + j].numbersArrays];
+                    // console.log('newNumbersArrays: ', newNumbersArrays);
+                    lines.push({ id: newId, numbersArrays: newNumbersArrays });
+                    // lines.sort((a,b) => a.id - b.id);
+                    console.log(lines);
+                }
             }
         }
-            
-        console.log('cardData: ', cardData);
 
-        // console.log(originalLines)
-        // console.log(newLines)
-        
-        // return totalScratchcards;
+        // lines.sort((a, b) => {
+        //     return a.id - b.id;
+        // });
+
+        console.log(lines);
+
+        return lines.length
+
         return 'incomplete'
     }
    
