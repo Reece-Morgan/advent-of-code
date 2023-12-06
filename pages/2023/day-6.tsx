6d24c594806866c203520a981271172b2ce3f96a
Reece <reecemorgandev@gmail.com>
2023-12-06 11:15:02 +0000

    day 6 complete

--- pages/2023/day-6.tsx
+++ pages/2023/day-6.tsx
@@ -1,5 +1,5 @@
 import { Title } from "@aoc/components"
-import { daySixData, testData } from "puzzle-inputs/2023/6";
+import { daySixData } from "puzzle-inputs/2023/6";
 import styled from "styled-components";
 
 const TwentyTwentyThree_DaySix = () => {
@@ -33,9 +33,33 @@ const TwentyTwentyThree_DaySix = () => {
         return timesBeatenRecord.reduce((accumulator, currentValue) => accumulator * currentValue);
     }
 
-    const partTwo = () => {
-        // Function for Part Two
-        return 'incomplete'
+    const partTwo = (data: string) => {
+        const lines = data.trim().split(/\n/g);
+        const time = lines[0].substring(lines[0].indexOf(':') + 1);
+        const distance = lines[1].substring(lines[1].indexOf(':') + 1);
+
+        const arrOfTime = time.split(/(\s+)/).filter(function(str) {
+            return /\S/.test(str);
+        });
+        const arrOfDistance = distance.split(/(\s+)/).filter(function(str) {
+            return /\S/.test(str);
+        });
+        
+        const totalTime = parseInt(arrOfTime.reduce((acc, current) => acc + current));
+        const totalDistance = parseInt(arrOfDistance.reduce((acc, current) => acc + current));
+        
+        let waysToBeatRecord = 0;
+        let holdTime = 0;
+        for (let i = 0; i < totalTime; i++) {
+            const remainingTime = totalTime - holdTime;
+            const distanceCovered = holdTime * remainingTime;
+            if (distanceCovered > totalDistance) {
+                waysToBeatRecord++;
+            }   
+            holdTime++;
+        }
+
+        return waysToBeatRecord;
     }
    
     return (
@@ -44,7 +68,7 @@ const TwentyTwentyThree_DaySix = () => {
             <Subtitle>Part 1</Subtitle>
             <Answer>{partOne(daySixData)}</Answer>
             <Subtitle>Part 2</Subtitle>
-            <Answer>{partTwo()}</Answer>
+            <Answer>{partTwo(daySixData)}</Answer>
         </>
     )
 }
