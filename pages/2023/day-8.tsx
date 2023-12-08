d53e0b7424e0142ee6447ad7965e02c52dcf8b82
Reece <reecemorgandev@gmail.com>
2023-12-08 12:58:36 +0000

    day 8 part 2 WiP

--- pages/2023/day-8.tsx
+++ pages/2023/day-8.tsx
@@ -1,5 +1,5 @@
 import { Title } from "@aoc/components"
-import { dayEightData, testData, testData2 } from "puzzle-inputs/2023/8";
+import { dayEightData, testData, testData2, testData3 } from "puzzle-inputs/2023/8";
 import styled from "styled-components";
 
 const TwentyTwentyThree_DayEight = () => {
@@ -38,8 +38,46 @@ const TwentyTwentyThree_DayEight = () => {
     }
 
     const partTwo = (data: string) => {
-        // Function for Part Two
-        return 'incomplete'
+        let numberOfSteps = 0;
+
+        const lines = data.trim().split(/\n/g).filter(item => item !== "");
+        const dir = lines.splice(0, 1);
+        const directions = dir[0].split('');
+        let currentLocation: string[] = [];
+
+        const locationArr: Locations[] = [];
+        lines.forEach(line => {
+            const matches = line.match(/(\w+) = \((\w+), (\w+)\)/);
+            if (matches) locationArr.push({current: matches[1], left: matches[2], right: matches[3]})
+        })
+
+        const startingLocations: string[] = [];
+        locationArr.forEach(loc => {
+            if (loc.current.endsWith('A')) startingLocations.push(loc.current);
+        })
+
+        currentLocation = startingLocations;
+
+        let endsWithZ = 0;
+        while (endsWithZ !== startingLocations.length) {
+            for (let d = 0; d < directions.length; d++) {
+                endsWithZ = 0;
+                for (let l = 0; l < startingLocations.length; l++) {
+                    console.log('Im trying!')
+                    const nextDir = directions[d];
+                    const index = locationArr.findIndex(item => item.current === currentLocation[l]);
+                    const currentItem = locationArr[index];
+                    const nextLocation = nextDir === 'L' ? currentItem.left : currentItem.right;
+                    const newIndex = locationArr.findIndex(item => item.current === nextLocation);
+                    const newCurrentItem = locationArr[newIndex];
+                    currentLocation[l] = newCurrentItem.current;
+                    numberOfSteps++;
+                    if (currentLocation[l].endsWith('Z')) endsWithZ++;
+                }
+            }
+        }
+
+        return numberOfSteps / startingLocations.length;
     }
    
     return (
@@ -48,7 +86,7 @@ const TwentyTwentyThree_DayEight = () => {
             <Subtitle>Part 1</Subtitle>
             <Answer>{partOne(dayEightData)}</Answer>
             <Subtitle>Part 2</Subtitle>
-            <Answer>{partTwo(testData)}</Answer>
+            <Answer>{partTwo(dayEightData)}</Answer>
         </>
     )
 }
