24ec6546cba1c994d8d41b6f6b4445666bc4257b
Reece <reecemorgandev@gmail.com>
2025-12-02 09:21:05 +0000

    day 3 setup

---
+++ app/2025/day-03/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import styled from "styled-components";
+import { Title } from "@aoc/components";
+// import { testData } from "./input";
+// import {  } from "./functions";
+
+const TwentyTwentyFive_Day3 = () => {
+  return (
+    <>
+      <Title>2025 - Day 2</Title>
+      <Subtitle>Part 1</Subtitle>
+      <Answer>incomplete</Answer>
+      <Subtitle>Part 2</Subtitle>
+      <Answer>incomplete</Answer>
+    </>
+  );
+};
+
+export default TwentyTwentyFive_Day3;
+
+const Subtitle = styled.h2`
+  text-align: center;
+`;
+
+const Answer = styled.div`
+  text-align: center;
+`;
